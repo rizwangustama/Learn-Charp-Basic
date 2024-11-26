8ff77d52612f79b1557f714e5d24f154adab4035
Rizwan Gustama <rizonegustama@gmail.com>
2024-11-26 17:04:04 +0700

    update fix

---
+++ OOP/Customer.cs
@@ -0,0 +1,8 @@
+namespace OOP;
+
+public class Customer
+{
+    public int customerId;
+    public string customerName;
+    public string customerAddreess;
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace OOP;
+
+public class Program
+{
+    public static void Main()
+    {
+        Customer c1, c2;
+        c1 = new Customer();
+        c2 = new Customer();
+        
+        Console.WriteLine(c1);
+        Console.WriteLine(c2);
+        
+    }
+}
